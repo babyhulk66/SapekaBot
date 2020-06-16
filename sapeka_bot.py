55d679b865fd2ce95ea3e04252fd1790a1a4e6e6
babyhulk66 <santzzz777@gmail.com>
2020-06-15 23:05:29 -0300

    remocao de comentario desnecessario

--- sapeka_bot.py
+++ sapeka_bot.py
@@ -4,7 +4,6 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
